6fe2616132247f4ca53dca7b657eb7b9665bf679
Bermuda_UT <hoso1312@gmail.com>
2017-08-19 19:24:33 +1000

    #5 #8 Testing update

--- app/src/androidTest/java/com/comp30022/helium/strawberry/ApplicationPropertyTest.java
+++ app/src/androidTest/java/com/comp30022/helium/strawberry/ApplicationPropertyTest.java
@@ -2,7 +2,10 @@ package com.comp30022.helium.strawberry;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.core.deps.guava.util.concurrent.ExecutionError;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,11 +19,28 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ApplicationPropertyTest {
+    private static final String TAG = ApplicationPropertyTest.class.getSimpleName();
+
+    public ApplicationPropertyTest() {
+        Log.i(TAG, "----------------------------------------\n" +
+                   "         Google Maps Unit Test\n"+
+                   "----------------------------------------\n");
+    }
     @Test
-    public void useAppContext() throws Exception {
+    public void packageName_isCorrect() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.comp30022.helium.strawberry", appContext.getPackageName());
+        Log.d(TAG, "[Test] Package Name is correct - START");
+
+        try {
+            assertEquals("com.comp30022.helium.strawberry", appContext.getPackageName());
+        } catch (Exception e) {
+            Log.e(TAG, "[Test] Package Name is correct - FAILURE");
+            Log.e(TAG, e.getMessage());
+            e.printStackTrace();
+        }
+
+        Log.i(TAG, "[Test] Package Name is correct - SUCCESS");
     }
 }
