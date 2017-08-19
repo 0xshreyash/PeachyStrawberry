b7ddd12d1b6aac6ba8e814260febd3ec10f1fefb
JJJJessie <guosiqi719@gmail.com>
2017-08-19 17:29:14 +1000

    #4 make the app connect with Facebook by using one login button

---
+++ app/src/main/java/com/comp30022/helium/strawberry/FacebookActivity.java
@@ -0,0 +1,12 @@
+package com.comp30022.helium.strawberry;
+
+import android.support.v7.app.AppCompatActivity;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+/**
+ * Created by jjjjessie on 19/8/17.
+ */
+
+public class FacebookActivity extends AppCompatActivity {
+}
