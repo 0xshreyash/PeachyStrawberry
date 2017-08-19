657f9afebd1332b32f88f4d85d84687b1dd47304
jiahong <jfong361@gmail.com>
2017-08-19 19:32:29 +1000

    Test vuforia imports with sample code from sample project; see #10

--- app/src/main/java/com/comp30022/helium/strawberry/MainActivity.java
+++ app/src/main/java/com/comp30022/helium/strawberry/MainActivity.java
@@ -1,13 +1,80 @@
 package com.comp30022.helium.strawberry;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+import com.vuforia.State;
+
+public class MainActivity extends Activity implements Controller {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+        Init init = new Init(this);
+        init.initAR(this, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                Toast.makeText(getApplicationContext(), getString(R.string.INIT_ERROR_NO_CAMERA_ACCESS), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public boolean doInitTrackers() {
+        return false;
+    }
+
+    @Override
+    public boolean doLoadTrackersData() {
+        return false;
+    }
+
+    @Override
+    public boolean doStartTrackers() {
+        return false;
+    }
+
+    @Override
+    public boolean doStopTrackers() {
+        return false;
+    }
+
+    @Override
+    public boolean doUnloadTrackersData() {
+        return false;
+    }
+
+    @Override
+    public boolean doDeinitTrackers() {
+        return false;
+    }
+
+    @Override
+    public void onInitARDone(VuforiaException e) {
+
+    }
+
+    @Override
+    public void onVuforiaUpdate(State state) {
+
     }
 }
