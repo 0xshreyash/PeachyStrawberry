ef5a7afe3f51c2c2d14a16fe32269bdfcb486930
jfong <jfong361@gmail.com>
2017-08-21 16:18:32 +1000

    #10 cleanup vuforia elements

--- app/src/main/java/com/comp30022/helium/strawberry/MainActivity.java
+++ app/src/main/java/com/comp30022/helium/strawberry/MainActivity.java
@@ -20,79 +20,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends Activity implements Controller {
-    private Init vuforiaInit;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Ask user for camera and storage permissions (needed by vuforia initializer)
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
-        } else {
-            initializeVuforia();
-        }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 0) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                initializeVuforia();
-            } else {
-                Toast.makeText(getApplicationContext(), getString(R.string.INIT_ERROR_NO_CAMERA_ACCESS), Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    private void initializeVuforia() {
-        vuforiaInit = new Init(this);
-        vuforiaInit.initAR(this, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-    }
-
-    @Override
-    public boolean doInitTrackers() {
-        return false;
-    }
-
-    @Override
-    public boolean doLoadTrackersData() {
-        return false;
-    }
-
-    @Override
-    public boolean doStartTrackers() {
-        return false;
-    }
-
-    @Override
-    public boolean doStopTrackers() {
-        return false;
-    }
-
-    @Override
-    public boolean doUnloadTrackersData() {
-        return false;
-    }
-
-    @Override
-    public boolean doDeinitTrackers() {
-        return false;
-    }
-
-    @Override
-    public void onInitARDone(VuforiaException e) {
-
-    }
-
-    @Override
-    public void onVuforiaUpdate(State state) {
-
-    }
 
     public void goToMap(View view) {
         Intent intent = new Intent(this, MapViewActivity.class);
