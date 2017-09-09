edfa47618e3df28e9a54f218a056ac9e20d8cb14
JJJJessie <guosiqi719@gmail.com>
2017-09-09 17:55:12 +1000

    #30 Move the functions to LocationService (half finished)

--- app/src/main/java/com/comp30022/helium/strawberry/services/LocationService.java
+++ app/src/main/java/com/comp30022/helium/strawberry/services/LocationService.java
@@ -1,24 +1,94 @@
 package com.comp30022.helium.strawberry.services;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.comp30022.helium.strawberry.ErrorActivity;
+import com.comp30022.helium.strawberry.R;
 import com.comp30022.helium.strawberry.entities.Friend;
 import com.comp30022.helium.strawberry.patterns.Publisher;
 import com.comp30022.helium.strawberry.patterns.Subscriber;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
 
-public class LocationService implements Publisher<Location> {
+import static com.comp30022.helium.strawberry.R.id.info;
+
+public class LocationService implements Publisher<Location>, LocationListener {
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private TextView info;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
+    private static LocationService instance;
 
     private List<Subscriber<Location>> subscribers; // all subscribers here
 
-    public LocationService() { /* handle initialization here */ }
-    public void setup() { /* handle setup here */ }
+    public static LocationService getInstance() throws NotInstantiatedException {
+        if (instance == null)
+            throw new NotInstantiatedException();
+        return instance;
+    }
+    public void setup(GoogleApiClient mGoogleApiClient) {
+        /* handle initialization here */
+        this.mGoogleApiClient = mGoogleApiClient;
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+        this.mGoogleApiClient.connect();
+        instance = this;
+    }
 
     public Location getDeviceLocation() {
         // this method should return this device's current location
-        return null;
+        return mLastLocation;
     }
+
+    public LocationRequest getRequest(){
+        return mLocationRequest;
+    }
+
+
+    public void onResume() {
+        mGoogleApiClient.connect();
+    }
+
+    public void onPause() {
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+    public void setNewLocation(Location location) {
+        mLastLocation = location;
+    };
+
+    @Override
+    public void onLocationChanged(Location location) {
+        setNewLocation(location);
+    }
+
+
 
     public Location getUserLocation(Friend user) {
         // this method should translate Friend (java Type) into information
