e208c8b0ad29728b6d579f8c3b4d31d17de3d255
jfong <jfong361@gmail.com>
2017-09-17 23:33:52 +1000

    remove redundant longlat conversion to coord

--- app/src/main/java/com/comp30022/helium/strawberry/entities/Coordinate.java
+++ app/src/main/java/com/comp30022/helium/strawberry/entities/Coordinate.java
@@ -4,35 +4,28 @@ package com.comp30022.helium.strawberry.entities;
 import android.location.Location;
 
 
+/**
+ * Difference between Coordinate and pre-built Vector2f is the fact that this class
+ * is storing coordinates in doubles (the precision is important in bearings).
+ */
 public class Coordinate {
     private double x;
     private double y;
-    private boolean isLatLong = false;
 
     public Coordinate(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public Coordinate(double x, double y, boolean isLatLong) {
-        this.x = x;
-        this.y = y;
-        this.isLatLong = isLatLong;
-    }
 
     public Coordinate(Coordinate c) {
         this.x = c.x;
         this.y = c.y;
-        this.isLatLong = c.isLatLong;
     }
 
     public Coordinate(Location location) {
         this.x = location.getLongitude();
         this.y = location.getLatitude();
-    }
-
-    public void setIsLatLong(boolean bool) {
-        this.isLatLong = bool;
     }
 
     public double getX() {
@@ -47,18 +40,17 @@ public class Coordinate {
     public void update(Coordinate other) {
         this.x = other.x;
         this.y = other.y;
-        this.isLatLong = other.isLatLong;
     }
 
     public Coordinate subtract(Coordinate other) {
-        return new Coordinate(this.x-other.x, this.y-other.y, this.isLatLong);
+        return new Coordinate(this.x-other.x, this.y-other.y);
     }
 
     public Coordinate normalize() {
         double distToOrigin = euclidDistance(new Coordinate(0, 0));
         double normalX = this.x / distToOrigin;
         double normalY = this.y / distToOrigin;
-        return new Coordinate(normalX, normalY, this.isLatLong);
+        return new Coordinate(normalX, normalY);
     }
 
     public double euclidDistance(Coordinate other) {
@@ -77,67 +69,13 @@ public class Coordinate {
         return "(" + x + ", " + y + ")";
     }
 
-    /**
-     * Converts current LogLat Coordinate system into a Cartesian Cartesian format
-     */
-    public void toLocalCart() {
-        if (this.isLatLong) {
-            this.isLatLong = false;
-            double tmp = this.x;
-            this.x = this.y;
-            this.y = tmp;
-        }
-    }
 
-    /**
-     * returns a new Coordinate in Cartesian format (from LongLat Coordinate)
-     * @return new Coordinate in cartesian from loglat coordinate
-     */
-    public Coordinate toCart() {
-        if (this.isLatLong) {
-            Coordinate coord = new Coordinate(this.y, this.x);
-            coord.isLatLong = false;
-            return coord;
-        } else {
-            // already in cartesian format
-            return new Coordinate(this);
-        }
-    }
-
-    public void toLocalLongLat() {
-        if (!this.isLatLong) {
-            this.isLatLong = true;
-            double tmp = this.x;
-            this.x = this.y;
-            this.y = tmp;
-        }
-    }
-
-
-    public Coordinate toLongLat() {
-        if (!this.isLatLong) {
-            Coordinate coord = new Coordinate(this.y, this.x);
-            coord.isLatLong = true;
-            return coord;
-        } else {
-            return new Coordinate(this);
-        }
-    }
 
     public Coordinate rotateDegree(double theta) {
         // make sure we're in cartesian space
         theta = Math.toRadians(theta);
-        if (this.isLatLong) {
-            this.toLocalCart();
-            double newX = this.x * Math.cos(theta) - y * Math.sin(theta);
-            double newY = this.x * Math.sin(theta) + y * Math.cos(theta);
-            Coordinate res = new Coordinate(newX, newY, this.isLatLong);
-            res.toLocalLongLat();
-            return res;
-        } else {
-            double newX = this.x * Math.cos(theta) - y * Math.sin(theta);
-            double newY = this.x * Math.sin(theta) + y * Math.cos(theta);
-            return new Coordinate(newX, newY, this.isLatLong);
-        }
+        double newX = this.x * Math.cos(theta) - y * Math.sin(theta);
+        double newY = this.x * Math.sin(theta) + y * Math.cos(theta);
+        return new Coordinate(newX, newY);
     }
 }
