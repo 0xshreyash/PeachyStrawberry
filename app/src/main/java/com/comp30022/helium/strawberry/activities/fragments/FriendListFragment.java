9b9f93178735665ba5c53d8cedf32411bc1cca2b
ShreyashPatodia <spatodia@student.unimelb.edu.au>
2017-09-30 17:30:16 +1000

    #58 Document code for Michael

--- app/src/main/java/com/comp30022/helium/strawberry/activities/fragments/FriendListFragment.java
+++ app/src/main/java/com/comp30022/helium/strawberry/activities/fragments/FriendListFragment.java
@@ -2,39 +2,75 @@ package com.comp30022.helium.strawberry.activities.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.comp30022.helium.strawberry.StrawberryApplication;
+import com.comp30022.helium.strawberry.components.friends.FriendListAdapter;
 import com.comp30022.helium.strawberry.entities.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by shreyashpatodia on 30/09/17.
- */
 
+/**
+ * Fragment which will display the list of friends when instantiated.
+ *
+ */
 public class FriendListFragment extends Fragment {
 
-    private RecyclerView mMessageRecycler;
-    //private MessageListAdapter mMessageAdapter;
+    private RecyclerView mFriendRecycler;
+    private FriendListAdapter mFriendAdapter;
+    private List<User> friends;
 
+    /**
+     * Called when the fragment is created.
+     * @param savedInstanceState the saved instance
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        List<User> friends = new ArrayList<>();
-
+        friends = new ArrayList<>();
+        // Get the application token,
+        String token = StrawberryApplication.getString("token");
+        // Connect to facebook and get the users.
         User firstFriend = new User("1", "Shreyash");
         User secondFriend = new User("2", "Harry");
 
         // TODO: Actually getString messages from somewhere.
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
-        //Log.d("Hi How are you?", "I am good ");
-        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
-        //mMessageRecycler.setHasFixedSize(true);
-        MessageListAdapter mMessageAdapter = new MessageListAdapter(this, messages);
-        //mMessageRecycler.setAdapter(mMessageAdapter);
-        mMessageRecycler.setAdapter(mMessageAdapter);
-        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    /**
+     * Called to assign a view to the FriendFragment
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return the created view.
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_friend_list, null);
+    }
+
+    /**
+     * Called once the view has been created, assign the recycler view and the adapter
+     * accordingly.
+     * @param view the view that was created for the friend.
+     * @param savedInstance the saved instance.
+     */
+    public void onViewCreated(View view, Bundle savedInstance) {
+
+        mFriendRecycler = (RecyclerView)view.findViewById(R.id.reyclerview_friend_list);
+
+        // TODO: Make sure we use getContext instead of view.getContext()
+        mFriendAdapter = new FriendListAdapter(getContext(), friends);
+
+        mFriendRecycler.setAdapter(mFriendAdapter);
+        mFriendRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
--- app/src/main/java/com/comp30022/helium/strawberry/components/friends/FriendListAdapter.java
+++ app/src/main/java/com/comp30022/helium/strawberry/components/friends/FriendListAdapter.java
@@ -19,38 +19,57 @@ import java.util.List;
  * A Recycler view is just like a list view but has additional features that
  * makes it faster and is why I used the RecyclerVieww
  */
-public class FriendsListAdapter extends RecyclerView.Adapter {
+public class FriendListAdapter extends RecyclerView.Adapter {
 
-    private Context mContext;
+    // The context
+    private Context context;
+
+    // List of friends
     private List<User> friendList;
 
+    // TODO: Use the ADDABLE_FRIEND AND ADDED_FRIEND THING LATER
     private static final int ADDABLE_FRIEND = 1;
     private static final int ADDED_FRIEND = 2;
 
+    // Makeshift friend for the demoonstration
+    private static final int FRIEND = 0;
 
-    public FriendsListAdapter() {
-        friendList = new ArrayList<>();
+    /**
+     * Constructor for the adapter
+     * @param context the context for the friend list
+     * @param friendList the actual list of friends we need to display
+     */
+    public FriendListAdapter(Context context, List<User> friendList) {
+        this.friendList = friendList;
+        this.context = context;
     }
+
+    /**
+     * Returns how many items are in the actual friend list.
+     * @return the number of friends
+     */
     @Override
     public int getItemCount() {
         //Log.e("Check", "" + mMessageList.size());
         return friendList.size();
     }
 
-    // Determines the appropriate ViewType according to the sender of the message.
+    /**
+     * Determines the appropriate ViewType according to the sender of the message.
+     *
+     */
     @Override
     public int getItemViewType(int position) {
-        User friend = friendList.get(position);
+        //User friend = friendList.get(position);
         // TODO: Check if the user if a friend or not. If the user is nto a friend
-
-        return ADDABLE_FRIEND;
+        return FRIEND;
     }
 
     // Inflates the appropriate layout according to the ViewType.
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view;
-
+        /*
         if(viewType == ADDABLE_FRIEND) {
             view = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.addable_friend, parent, false);
@@ -61,8 +80,12 @@ public class FriendsListAdapter extends RecyclerView.Adapter {
                     .inflate(R.layout.added_friend, parent, false);
             return new FriendHolder(view);
         }
+        */
+
         //Log.e("Check", "Returning null");
-        return null;
+        view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.friend, parent, false);
+        return new FriendHolder();
     }
 
     // Passes the message object to a ViewHolder so that the contents can be bound to UI.
@@ -97,10 +120,11 @@ public class FriendsListAdapter extends RecyclerView.Adapter {
         }
 
         void bind(User friend) {
-            userNameText.setText(friend.getName());
+            userNameText.setText(friend.getUsername());
         }
     }
 
+    /*
     private class AddableFriendHolder extends FriendHolder{
 
         Button addFriendButton;
@@ -117,4 +141,5 @@ public class FriendsListAdapter extends RecyclerView.Adapter {
             // TODO: Profile Picture
         }
     }
+    */
 }
