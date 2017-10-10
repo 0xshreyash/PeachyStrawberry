45eba79520cfa6bcc91e0e2c9beaf4fabf03e691
ShreyashPatodia <spatodia@student.unimelb.edu.au>
2017-10-10 23:42:45 +1100

    #84 Rehaul StrawberryMapWrapperLayout

--- app/src/main/java/com/comp30022/helium/strawberry/components/map/StrawberryMapWrapperLayout.java
+++ app/src/main/java/com/comp30022/helium/strawberry/components/map/StrawberryMapWrapperLayout.java
@@ -1,56 +1,82 @@
 package com.comp30022.helium.strawberry.components.map;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
 import android.content.Context;
 import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.Marker;
-
-/**
- * Wrap map fragment in a relative layout, to be able to get the
- * offset.
- */
 public class StrawberryMapWrapperLayout extends RelativeLayout {
 
     private GoogleMap map;
-    private int offsetPixels;
-    private Marker marker;
-    private View clickableView;
 
+    /**
+     * Offset needed to position the clickableView
+     */
+    private int offsetPixels;
+
+    private Marker marker;
+
+    /**
+     * Our custom view which is returned from either the InfoWindowAdapter.getInfoContents
+     * or InfoWindowAdapter.getInfoWindow
+     */
+    private View infoWindow;
 
     public StrawberryMapWrapperLayout(Context context) {
         super(context);
     }
 
-    public void init(GoogleMap map, int offsetPixels) {
-        this.map = map;
-        this.offsetPixels = offsetPixels;
+    public StrawberryMapWrapperLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
 
+    public StrawberryMapWrapperLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    /**
+     * Must be called onMapReady?
+     */
+    public void init(GoogleMap map, int bottomOffsetPixels) {
+        this.map = map;
+        this.offsetPixels = bottomOffsetPixels;
+    }
+
+    /**
+     * Called from InfoWindowAdapter.getInfoContents
+     */
+    public void setMarkerWithInfoWindow(Marker marker, View infoWindow) {
+        this.marker = marker;
+        this.infoWindow = infoWindow;
+    }
+
+    /**
+     * Important because this dispatches the touchEvent on clicking the window
+     * @param originalEvent
+     * @return
+     */
     @Override
-    public  boolean dispatchTouchEvent(MotionEvent originalEvent) {
-        boolean toReturn = false;
+    public boolean dispatchTouchEvent(MotionEvent originalEvent) {
+        boolean ret = false;
+
         if(map != null) {
             if (marker != null && marker.isInfoWindowShown()) {
-                if (clickableView != null) {
-                    Point point = map.getProjection().toScreenLocation(marker.getPosition());
+                if (infoWindow != null) {
+                    Point mapPoint = map.getProjection().toScreenLocation(marker.getPosition());
 
-                    MotionEvent newE = MotionEvent.obtain(originalEvent);
+                    MotionEvent adjustedEvent = MotionEvent.obtain(originalEvent);
+                    adjustedEvent.offsetLocation(-mapPoint.x + (infoWindow.getWidth() / 2),
+                            -mapPoint.y + infoWindow.getHeight() + offsetPixels);
 
-                    newE.offsetLocation(
-                            -point.x + clickableView.getWidth() / 2.0f,
-                            -point.y + clickableView.getHeight() + offsetPixels
-                    );
-
-                    toReturn = clickableView.dispatchTouchEvent(newE);
+                    ret = infoWindow.dispatchTouchEvent(adjustedEvent);
                 }
             }
         }
-        return toReturn || super.dispatchTouchEvent(originalEvent);
+        return ret || super.dispatchTouchEvent(originalEvent);
     }
 }
