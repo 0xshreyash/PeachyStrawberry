42219323f1a85a65dfda4716c557f72feba70bd2
ShreyashPatodia <spatodia@student.unimelb.edu.au>
2017-10-22 14:07:46 +1100

    Add date for auth token expiry in comments

--- app/src/androidTest/java/com/comp30022/helium/strawberry/mocks/MockPeachServerInterface.java
+++ app/src/androidTest/java/com/comp30022/helium/strawberry/mocks/MockPeachServerInterface.java
@@ -9,7 +9,7 @@ import com.comp30022.helium.strawberry.patterns.Subscriber;
  */
 
 public class MockPeachServerInterface extends PeachServerInterface{
-    // Shreyash's information
+    // Shreyash's information, valid until 20th December, 2017. If test is failing this is probably why.
     public final static String SAMPLE_FB_TOKEN = "EAAJ5bdPqZCIIBAPO77SesRM0t8Ec7llEFdbjTZCwJe1ZAPo4FZAkrdHeZCFiKxE8xMhf0b8ZB4wAdZCyrNib7jZBUzkwChqqnEVoabcN5jaf7xOg7oSENSC7MAwI8piQV40i0UAPbuEdqQjNob89V53WXJhLf2DsBy8ZD";
     public final static String SAMPLE_ID_SELF = "59cf7b3c5a52f46d982b340a";
 
